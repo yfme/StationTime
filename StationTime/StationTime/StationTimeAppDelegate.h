816be402e7cfa684f6e3308f32686201907be22e
Fei Yang <yangfei.me@gmail.com>
2011-07-23 11:25:40 +0800

    README, gitignore, Navigation-Based Application.

---
+++ StationTime/StationTime/StationTimeAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  StationTimeAppDelegate.h
+//  StationTime
+//
+//  Created by yangfei on 11-7-23.
+//  Copyright 2011年 appxyz.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StationTimeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ StationTime/StationTime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StationTime
+//
+//  Created by yangfei on 11-7-23.
+//  Copyright 2011年 appxyz.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
